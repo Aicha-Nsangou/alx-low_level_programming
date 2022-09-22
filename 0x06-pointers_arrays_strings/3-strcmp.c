836564c3df9ecf9feb42acf3b9cdb0d6d8f961aa
Aicha-Nsangou <Anmanjoya@gmail.com>
2022-09-22 04:00:11 +0100

    function that compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,7 +17,10 @@ int i;
 
 for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
-	return (s1[i] != s2[i]) ? (s1[i] - s2[i]) : 0;
+	if (s1[i] != s2[i])
+	{
+		return (s1[i] - s2[i]);
+	}
 }
-
+return (0);
 }
